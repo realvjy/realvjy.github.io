08428d1cab29e4918cdc58e8db9a30228ba9e3f4
realvjy <realvjy@gmail.com>
2020-09-13 02:14:05 +0530

    fix subscribe

--- src/components/story-list.js
+++ src/components/story-list.js
@@ -83,21 +83,21 @@ const WorkList = () => {
               <p>Subscribe to the newsletter to get exclusive updates & digital art inspiration in your inbox.</p>
 
               <div id="mc_embed_signup">
-                <form action="https://vijayverma.us14.list-manage.com/subscribe/post?u=ed1f62133209766c02f08fd47&amp;id=c4285438c3" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+                <form action="https://designletter.us10.list-manage.com/subscribe/post?u=ac670d4650872310b44fc59b4&amp;id=2728fe6fff" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
                   <div id="mc_embed_signup_scroll" className="newsletter">
                     <div className="mc-field-group">
-                    	<label for="mce-EMAIL" style={{display:`none`}}>Email Address </label>
+                      <label for="mce-EMAIL" style={{display:`none`}}>Email Address </label>
                         <div className="inputbox">
-                    	   <input type="email" placeholder="Your email address please" name="EMAIL" className="required email" id="mce-EMAIL" />
+                         <input type="email" placeholder="Your email address please" name="EMAIL" className="required email" id="mce-EMAIL" />
                         </div>
                     </div>
                     <div className="sub-action">
                       <div id="mce-responses" className="clear">
-                      		<div className="response" id="mce-error-response" style={{display:`none`}}></div>
-                      		<div clasNames="response" id="mce-success-response" style={{display:`none`}}></div>
+                          <div className="response" id="mce-error-response" style={{display:`none`}}></div>
+                          <div clasNames="response" id="mce-success-response" style={{display:`none`}}></div>
                       </div>
                       <div style={{position: `absolute`, left:`-5000px`}}aria-hidden="true">
-                        <input type="text" name="b_ed1f62133209766c02f08fd47_c4285438c3" tabindex="-1" value="" />
+                        <input type="text" name="b_ac670d4650872310b44fc59b4_2728fe6fff" tabindex="-1" value="" />
                       </div>
                       <div className="clear">
                           <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button sub-button" />
