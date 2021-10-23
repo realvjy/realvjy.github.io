b9fe9660e9b1d3237940dc8790193d04653a69c0
realvjy <realvjy@gmail.com>
2021-10-23 15:03:08 +0530

    3dicons fix

--- src/components/featured.js
+++ src/components/featured.js
@@ -14,16 +14,16 @@ const Featured = () => {
           <div className="projects">
             <div className="project-box">
               <div className="p-wrap">
-                <img src="3dicons-cover-2.png" className="tdicons"/>
-                <span className="new-tag">SOON</span>
+                <a href="http://3dicons.co/?ref=vijayverma.co"><img src="3dicons-cover-2.png" className="tdicons"/></a>
+                <span className="new-tag">NEW</span>
                 <h3>3dicons</h3>
                 <p>3Dicons is a set of 120+ beautifully crafted open-source icons that have been created in Blender and made available to the community. Under CC0 license, source file FREE. </p>
-                <a href="http://3dicons.co/?ref=vijayverma.co" target="_blank">Early access ›</a>
+                <a href="http://3dicons.co/?ref=vijayverma.co" target="_blank">explore here ›</a>
               </div>
             </div>
             <div className="project-box">
               <div className="p-wrap">
-                <img src="illlustrations.png" className="illlus"/>
+                <a href="http://illlustrations.co/?ref=vijayverma.co"><img src="illlustrations.png" className="illlus"/></a>
                 <h3>illlustrations</h3>
                 <p>Open source illustrations kit. More than 100+ illustrations to design awesome landing page, mockup and presentations. You can download completely free.</p>
                 <a href="http://illlustrations.co/?ref=vijayverma.co" target="_blank">explore here ›</a>
