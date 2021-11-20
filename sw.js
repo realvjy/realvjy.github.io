bc13c319a52cda790394e1b655c3d4be37366eb3
realvjy <realvjy@gmail.com>
2021-11-20 20:15:03 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-6f52e698234f2670e6f0.js"
+    "url": "webpack-runtime-af4f76af0ff0ef5a74b6.js"
   },
   {
     "url": "framework-c8d3a5345e3f321d7da5.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "33ec748f67d04ec450e69de1334e1eec"
+    "revision": "ac124215ae0e1725c1b9c9101b354101"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
