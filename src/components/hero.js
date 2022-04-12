b78cc3c148e7a26595024df72b0580e5a7a68205
realvjy <realvjy@gmail.com>
2022-04-13 02:00:39 +0530

    title fix

--- src/components/hero.js
+++ src/components/hero.js
@@ -21,7 +21,7 @@ const Hero = () => {
               </div>
 
               <div className="short-info">
-                <p>Hi! I’m <a href="/about">vijay verma</a>. A design wizard voyaging into the metaverse. I tell the story through my design and illustrations. I spent most of my time creating designs for <a href="/work">brands</a>  and creating open-source design <a href="/work">resources</a>. </p>
+                <p>Hi! I’m <a href="/about">vijay verma</a>. A design wizard voyaging into the metaverse. I tell the story through my design and illustrations. I spent most of my time designing for <a href="/work">brands</a>  and creating open-source design <a href="/work">resources</a>. </p>
               </div>
               <div className="btns">
                 <a className="btn" href="mailto:hello@vijayverma.co?subject=Saying Hello!!!">say hello!</a>
